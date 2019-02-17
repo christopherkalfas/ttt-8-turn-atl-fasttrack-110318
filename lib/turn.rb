ae83c2f085bb6c46544a4af2d497ef33210c9275
Christopher Kalfas <chriskalfas@yahoo.com>
2019-02-17 17:58:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,13 +6,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def valid_move?(board, index)
-  !position_taken?(board, index) && index.between?(0,8)
-end
+
 
 
 def position_taken?(board, index)
