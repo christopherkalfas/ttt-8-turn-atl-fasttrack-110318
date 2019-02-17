0030f777891e20bc760cb8ea0db12383c836c01f
Christopher Kalfas <chriskalfas@yahoo.com>
2019-02-17 18:05:19 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,8 @@ def turn(board)
   index = input_to_index(user_input)
  
   if valid_move?(board, index)
-    move(board, input_to_index(user_input), current_player)
+    move(board, index)
+    display_board(board)
   else turn(board)
   end
 end
